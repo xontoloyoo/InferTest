2448d6afeff34e3ce4b859fb429395bd2c9f6de4
xontoloyo <143369114+xontoloyoo@users.noreply.github.com>
2023-09-30 22:25:33 +0700

    Update tes.py

--- src/tes.py
+++ src/tes.py
@@ -178,12 +178,16 @@ def preprocess_song(song_input, mdx_model_params, song_id, is_webui, input_type,
     song_output_dir = os.path.join(output_dir, song_id)
     orig_song_path = convert_to_stereo(orig_song_path)
 
-    display_progress('[~] Separating Vocals...', 0.1, is_webui, progress)
-    vocals_path = run_mdx(mdx_model_params, song_output_dir, os.path.join(mdxnet_models_dir, 'UVR-MDX-NET-Voc_FT.onnx'), orig_song_path, denoise=True, keep_orig=keep_orig)
+    #display_progress('[~] Separating Vocals...', 0.1, is_webui, progress)
+    #vocals_path = run_mdx(mdx_model_params, song_output_dir, os.path.join(mdxnet_models_dir, 'UVR-MDX-NET-Voc_FT.onnx'), orig_song_path, denoise=True, keep_orig=keep_orig)
    
-    display_progress('[~] Applying DeReverb to Vocals...', 0.3, is_webui, progress)
-    vocals_path = run_mdx(mdx_model_params, song_output_dir, os.path.join(mdxnet_models_dir, 'Reverb_HQ_By_FoxJoy.onnx'), vocals_path, invert_suffix='DeReverb', exclude_main=True, denoise=True)
+    #display_progress('[~] Applying DeReverb to Vocals...', 0.3, is_webui, progress)
+    #vocals_dereverb_path = run_mdx(mdx_model_params, song_output_dir, os.path.join(mdxnet_models_dir, 'Reverb_HQ_By_FoxJoy.onnx'), vocals_path, invert_suffix='DeReverb', exclude_main=True, denoise=True)
 
+    display_progress('[~] Applying DeReverb to Vocals...', 0.3, is_webui, progress)
+    vocals_dereverb_path = run_mdx(mdx_model_params, song_output_dir, os.path.join(mdxnet_models_dir, 'Reverb_HQ_By_FoxJoy.onnx'), orig_song_path, invert_suffix='DeReverb', exclude_main=True, denoise=True)
+
+    
     return orig_song_path, vocals_path, instrumentals_path, vocals_dereverb_path
 
 
