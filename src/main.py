0c1e9af01b5320abdba7454538948da1977dfa02
xontoloyo <143369114+xontoloyoo@users.noreply.github.com>
2023-10-02 10:59:39 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -286,17 +286,17 @@ def song_cover_pipeline(song_input, voice_model, pitch_change, keep_files,
         ai_vocals_path = os.path.join(song_dir, f'{os.path.splitext(os.path.basename(orig_song_path))[0]}_{voice_model}_p{pitch_change}_i{index_rate}_fr{filter_radius}_rms{rms_mix_rate}_pro{protect}_{f0_method}{"" if f0_method != "mangio-crepe" else f"_{crepe_hop_length}"}.wav')
         ai_cover_path = os.path.join(song_dir, f'{os.path.splitext(os.path.basename(orig_song_path))[0]} (Versi {voice_model}).{output_format}')
             
-            if not os.path.exists(ai_vocals_path):
-                display_progress('[~] Converting voice using RVC...', 0.5, is_webui, progress)
-                voice_change(voice_model, vocals_dereverb_path, ai_vocals_path, pitch_change, f0_method, index_rate, filter_radius, rms_mix_rate, protect, crepe_hop_length, is_webui)
+        if not os.path.exists(ai_vocals_path):
+            display_progress('[~] Converting voice using RVC...', 0.5, is_webui, progress)
+            voice_change(voice_model, vocals_dereverb_path, ai_vocals_path, pitch_change, f0_method, index_rate, filter_radius, rms_mix_rate, protect, crepe_hop_length, is_webui)
             
             display_progress('[~] Applying audio effects to Vocals...', 0.8, is_webui, progress)
             ai_vocals_mixed_path = add_audio_effects(ai_vocals_path, reverb_rm_size, reverb_wet, reverb_dry, reverb_damping)
             
-            if pitch_change_all != 0:
-                display_progress('[~] Applying overall pitch change', 0.85, is_webui, progress)
-                instrumentals_path = pitch_shift(instrumentals_path, pitch_change_all)
-                backup_vocals_path = pitch_shift(backup_vocals_path, pitch_change_all)
+        if pitch_change_all != 0:
+            display_progress('[~] Applying overall pitch change', 0.85, is_webui, progress)
+            instrumentals_path = pitch_shift(instrumentals_path, pitch_change_all)
+            backup_vocals_path = pitch_shift(backup_vocals_path, pitch_change_all)
 
 ######
     #    display_progress('[~] Combining AI Vocals and Instrumentals...', 0.9, is_webui, progress)
