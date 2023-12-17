5e31fc37d92163079c93beeb3dc92d852bfd0955
xontoloyo <143369114+xontoloyoo@users.noreply.github.com>
2023-12-17 11:22:16 +0700

    Update download_models.py

--- src/download_models.py
+++ src/download_models.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 import requests
 
-MDX_DOWNLOAD_LINK = 'https://github.com/TRvlvr/model_repo/releases/download/all_public_uvr_models/'
+#MDX_DOWNLOAD_LINK = 'https://github.com/TRvlvr/model_repo/releases/download/all_public_uvr_models/'
+MDX_DOWNLOAD_LINK = 'https://huggingface.co/lj1995/VoiceConversionWebUI/tree/main/uvr5_weights/'
 RVC_DOWNLOAD_LINK = 'https://huggingface.co/lj1995/VoiceConversionWebUI/resolve/main/'
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -17,11 +18,18 @@ def dl_model(link, model_name, dir_name):
                 f.write(chunk)
 
 
+#if __name__ == '__main__':
+#    mdx_model_names = ['UVR-MDX-NET-Voc_FT.onnx', 'UVR_MDXNET_KARA_2.onnx', 'Reverb_HQ_By_FoxJoy.onnx']
+#    for model in mdx_model_names:
+#        print(f'Downloading {model}...')
+#        dl_model(MDX_DOWNLOAD_LINK, model, mdxnet_models_dir)
+
 if __name__ == '__main__':
-    mdx_model_names = ['UVR-MDX-NET-Voc_FT.onnx', 'UVR_MDXNET_KARA_2.onnx', 'Reverb_HQ_By_FoxJoy.onnx']
+    mdx_model_names = ['HP5_only_main_vocal.pth', 'VR-DeEchoDeReverb.pth']
     for model in mdx_model_names:
         print(f'Downloading {model}...')
         dl_model(MDX_DOWNLOAD_LINK, model, mdxnet_models_dir)
+    
 
     rvc_model_names = ['hubert_base.pt', 'rmvpe.pt']
     for model in rvc_model_names:
